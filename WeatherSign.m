a0def966f40496bf4bce4c771c7855bc4caa6231
xjy13 <sucka0418@gmail.com>
2017-06-24 20:25:29 +0800

    add weather sign

--- WeatherSign.m
+++ WeatherSign.m
@@ -21,15 +21,15 @@ UIImage *img;
     NSMutableArray *weatherArray = [[NSMutableArray alloc]init];
     weatherArray = [NSMutableArray array];
     [weatherArray addObject:[GetWeather getWeather:@"Taipei"]];
-    NSLog(@"XDDDDD1 = %@",[weatherArray objectAtIndex:0]);
-    NSLog(@"XDDDDD2 = %@",[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"temp_c"] );
-//    city = [[[[weatherArray objectAtIndex:0] objectForKey:@"location"] objectForKey:@"name"] stringByAppendingString:[[[weatherArray objectAtIndex:0] objectForKey:@"location"] objectForKey:@"country"]];
-//    NSLog(@"city name = %@",city);
-    degree = [NSString stringWithFormat:@"%@ C",[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"temp_c"]];
-//    NSLog(@"degree = %@",degree);
-//    NSString *imgURL = [[[weatherArray objectAtIndex:0]objectForKey:@"condition"] objectForKey:@"icon"];
-//    img = [[UIImage alloc]init];
-//    img= [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imgURL]]];
+//    NSLog(@"XDDDDD1 = %@",[weatherArray objectAtIndex:0]);
+//    NSLog(@"XDDDDD2 = %@",[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"temp_c"] );
+//    NSLog(@"XDDDDD3 = %@",[[[weatherArray objectAtIndex:0]objectForKey:@"location"] objectForKey:@"name"]);
+//    NSLog(@"XDDDDD4img = %@",[[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"condition"] objectForKey:@"icon"] );
+    city = [[[weatherArray objectAtIndex:0]objectForKey:@"location"] objectForKey:@"name"];
+    degree = [NSString stringWithFormat:@"%@ ºC / %@ ºF",[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"temp_c"],[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"temp_f"]];
+    NSString *imgURL = [@"https:" stringByAppendingString:[[[[weatherArray objectAtIndex:0] objectForKey:@"current"] objectForKey:@"condition"] objectForKey:@"icon"]];
+    img = [[UIImage alloc]init];
+    img= [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imgURL]]];
   
 }
 
